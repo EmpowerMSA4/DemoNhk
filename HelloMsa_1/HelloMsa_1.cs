f8ba3435d628447546d8318673b3f4f72d580aab
SKYLINE2\MichielSA <michiel.saelen@skyline.be>
2024-02-25 11:07:32 +0800

    Retrieve interfaces

--- HelloMsa_1/HelloMsa_1.cs
+++ HelloMsa_1/HelloMsa_1.cs
@@ -3,9 +3,11 @@ namespace HelloMsa_1
 	using System;
 	using System.Collections.Generic;
 	using System.Globalization;
+	using System.Linq;
 	using System.Text;
 	using Skyline.DataMiner.Automation;
-	
+	using Skyline.DataMiner.Core.DataMinerSystem.Automation;
+
 	/// <summary>
 	/// Represents a DataMiner Automation script.
 	/// </summary>
@@ -17,8 +19,17 @@ namespace HelloMsa_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-			// The next line will generate an information event.
-			engine.GenerateInformation("Hello Michiel");
+			// Get DataMiner System
+			var thisDms = engine.GetDms();
+
+			// Get number of interfaces for every switch
+			var switches = thisDms.GetElements().Where(e => e.Protocol.Name == "Mellanox Technologies MLNX-OS Manager");
+			foreach ( var mellanox in switches)
+			{
+				var interfaceTable = mellanox.GetTable(1100);
+				var keys = interfaceTable.GetPrimaryKeys();
+				engine.GenerateInformation($"Switch {mellanox.Name} found with {keys.Count()} interfaces");
+			}
 		}
 	}
 }
